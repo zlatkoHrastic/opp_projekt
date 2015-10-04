60f3f7c521a0c5191eb9001d6d7fbd6a94f0fe6c
Querculus <zlatko.hr8@gmail.com>
2015-10-04 15:04:18 +0200

    comitaj sve

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms252');
+    define('DB_USER', 'simplecmsuser252');
+    define('DB_PASSWORD', 'eZw&&sb%pxD]');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'zlatko.hrastic');
+    define('DEFAULT_ADMIN_PASSWORD', 'ferovac');
+?>
